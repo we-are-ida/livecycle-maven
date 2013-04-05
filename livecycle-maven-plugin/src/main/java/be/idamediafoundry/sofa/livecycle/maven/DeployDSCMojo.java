c54abe59ec1882756ecc162dfb6f947e4e05e51f
Filip Maelbrancke <filip@maelbrancke.net>
2013-04-05 18:34:12 +0200

    pom files & copyrights updated

--- livecycle-maven-plugin/src/main/java/be/idamediafoundry/sofa/livecycle/maven/DeployDSCMojo.java
+++ livecycle-maven-plugin/src/main/java/be/idamediafoundry/sofa/livecycle/maven/DeployDSCMojo.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012 iDA MediaFoundry (www.ida-mediafoundry.be)
+ * Copyright 2012-2013 iDA MediaFoundry (www.ida-mediafoundry.be)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
